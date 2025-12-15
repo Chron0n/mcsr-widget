080840b2bba67188d6f5c5c7c0ba58991aeacda0
Chron0n <125386517+Chron0n@users.noreply.github.com>
2025-12-16 00:35:13 +0100

    Fix preloadImage path in config.js
    Updated preloadImage path to use BASE_URL.

--- src/stores/config.js
+++ src/stores/config.js
@@ -8,13 +8,15 @@ export const useConfigStore = defineStore('config', () => {
 
   const nickname = route.query.nickname
   preloadImage(`https://mineskin.eu/helm/${nickname}/100.png`)
-  preloadImage('/icons/ranked.png')
+  preloadImage(`${import.meta.env.BASE_URL}icons/ranked.png`)
 
   const badge = Number(route.query.badge)
   const rate = Number(route.query.rate)
   const accent = '#' + route.query.accent
   const state = Number(route.query.state)
   const leaderboard = route.query.leaderboard === 'true'
+
+  console.log('Leaderboard:', leaderboard)
 
   const isExpanded = ref(false)
   const isExtra = ref(false)
