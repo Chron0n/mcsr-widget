88bbabfd015a78aab85e70d51da90e999e884704
Chron0n <125386517+Chron0n@users.noreply.github.com>
2025-12-16 00:22:20 +0100

    Add base path configuration to Vite config

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import svgLoader from 'vite-svg-loader'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/mcsr-widget/',
   plugins: [vue(), vueDevTools(), svgLoader()],
   resolve: {
     alias: {
